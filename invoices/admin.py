6dee6af8eef57734220c9a5ad618f4d02dc6aad2
JennyJudova <jenny@Jennys-MacBook-Pro.local>
2019-11-13 12:26:40 +0000

    3 models working

--- invoices/admin.py
+++ invoices/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Invoice
+from .models import Invoice, Client, Invoice_Item
 
 admin.site.register(Invoice)
+admin.site.register(Client)
+admin.site.register(Invoice_Item)
--- invoices/models.py
+++ invoices/models.py
@@ -2,6 +2,22 @@ from datetime import date
 from djmoney.models.fields import MoneyField
 from django.db import models
 
+class Client(models.Model):
+    full_name = models.CharField(max_length=50, blank=True)
+    company_name = models.CharField(max_length=50, blank=True)
+    address = models.CharField(max_length=100, blank=True)
+    email = models.CharField(max_length=50)
+
+
+class Invoice_Item(models.Model):
+    item_description = models.CharField(max_length=200, unique=True)
+    quantity_hrs = models.FloatField(null=True, default=0)
+    unit_price_hrs = models.FloatField(null=True, default=0)
+    total = MoneyField(max_digits=10, decimal_places=2, null=True, default_currency='GBP')
+
+    def __str__(self):
+        return f'{self.item_description}'
+
 class Invoice(models.Model):
     invoice_number = models.CharField(max_length=50, unique=True)
     issue_date = models.DateField(default=date.today)
@@ -14,6 +30,55 @@ class Invoice(models.Model):
     terms = models.CharField(max_length=500, blank=True)
     is_paid = models.BooleanField(default=False)
     currency = models.CharField(max_length=50, default='GBP')
+    invoice_items = models.ForeignKey(
+        Invoice_Item,
+        related_name='invoice',
+        on_delete=models.DO_NOTHING,
+        null=True
+    )
+    client = models.ForeignKey(
+        Client,
+        related_name='invoice',
+        on_delete=models.DO_NOTHING,
+        null=True
+    )
 
     def __str__(self):
         return f'{self.invoice_number}'
+
+
+
+
+# class Line(models.Model):
+#     name = models.CharField(max_length=50)
+
+#     def __str__(self):
+#         return f'{self.name}'
+
+# # The thing you are referencing should be created first so Station references Zone so Zone sits above Station
+# class Zone(models.Model):
+#     zone = models.IntegerField()
+
+#     def __str__(self):
+#         return f'{self.zone}'
+
+# # Create your models here.
+# class Station(models.Model):
+#     name = models.CharField(max_length=50, unique=True)
+#     lat = models.FloatField()
+#     lon = models.FloatField()
+#     is_night_tube = models.BooleanField(default=False)
+#     zone = models.ForeignKey(
+#         Zone,
+#         related_name='stations',
+#         on_delete=models.DO_NOTHING,
+#         null=True
+#     )#prepares the flip relationship so that they can refencence each other.
+#     lines = models.ManyToManyField(
+#         Line,
+#         related_name='stations',
+#         blank=True
+#     )
+
+#     def __str__(self):
+#         return f'{self.name}'
