8af46cb5793afd56695a5c0e75798d0caccadbd4
JennyJudova <jenny@Jennys-MacBook-Pro.local>
2019-11-13 16:20:47 +0000

    new create and update methods added

--- invoices/serializers.py
+++ invoices/serializers.py
@@ -1,3 +1,5 @@
+#pylint: disable=no-member
+
 from rest_framework import serializers
 from .models import Invoice, Invoice_Item, Client
 
@@ -45,6 +47,41 @@ class InvoiceSerializer(serializers.ModelSerializer):
 
     invoice_items = NestedInvoice_ItemSerializer(many=True)
     client = NestedClientSerializer()
+
+    def create(self, data):
+        invoice_items_data = data.pop('invoice_items')
+        client_data = data.pop('client')
+
+        invoice = Invoice(**data)
+        invoice.client = Client.objects. get(**client_data)
+        invoice_items = [Invoice_Item.objects.get(**invoice_item_data) for invoice_item_data in invoice_items_data]
+
+        invoice.save()
+        invoice.invoice_items.set(invoice_items)
+        return invoice
+
+    def update(self, invoice, data):
+        invoice_items_data = data.pop('invoice_items')
+        client_data = data.pop('client')
+
+        invoice.invoice_number = data.get('invoice_number', invoice.invoice_number)
+        invoice.issue_date = data.get('issue_date', invoice.issue_date)
+        invoice.due_date = data.get('due_date', invoice.due_date)
+        invoice.vat_registered = data.get('vat_registered', invoice.vat_registered)
+        invoice.subtotal = data.get('subtotal', invoice.subtotal)
+        invoice.vat = data.get('vat', invoice.vat)
+        invoice.total = data.get('total', invoice.total)
+        invoice.notes = data.get('notes', invoice.notes)
+        invoice.terms = data.get('terms', invoice.terms)
+        invoice.is_paid = data.get('is_paid', invoice.is_paid)
+        invoice.currency = data.get('currency', invoice.currency)
+
+        invoice.client = Client.objects. get(**client_data)
+        invoice_items = [Invoice_Item.objects.get(**invoice_item_data) for invoice_item_data in invoice_items_data]
+
+        invoice.save()
+        invoice.invoice_items.set(invoice_items)
+        return invoice
 
     class Meta:
         model = Invoice
